bf8db360ca80b5d11d783194d045681037c7c3be
JacobDangCS50 <jacobohdang@gmail.com>
2023-08-01 00:25:32 +0000

    created components and general styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-function App() {
+import React from 'react';
+import styles from './style';
+import { NavBar, Hero, Services, About, Testimonials, Contact, Footer } from './components'
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <NavBar />
+      </div>
     </div>
-  );
+
+    <div className={`${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Services />
+        <About />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </div>
+    </div>
+  </div>
+  )
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      //implement colors HERE (primary, secondary, etc.)
+    },
+    /* Implement fonts here
+    fontFamily: {
+      font: [],
+    },
+    */
+  },
+  // Adjusts screen size according to browse size
+  screens: {
+    xs: "480px",
+    ss: "620px",
+    sm: "768px",
+    md: "1060px",
+    lg: "1200px",
+    xl: "1700px",
+  },
+  plugins: [],
+}
+
