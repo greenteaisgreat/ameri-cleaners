6980a73a3de5cc2228631165db03d274fbecf420
JacobDangCS50 <jacobohdang@gmail.com>
2023-08-04 21:27:13 +0000

    additional features implemented

--- src/components/Eco.jsx
+++ src/components/Eco.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Eco = () => {
   return (
-    <div id="eco">Eco</div>
+    <div id="eco">
+      <div className="mx-auto px-2 py-20">
+        <div className="flex flex-col md:flex-row items-center">
+          <div className="md:w-1/2 md:pr-8">
+            <h3 className="text-4xl md:text-5xl font-bold mb-6">Eco-Friendly Dry Cleaning</h3>
+            <p className="text-gray-700 text-lg leading-relaxed font-poppins">
+            We are extremely environmentally conscious and always make sure that we are using only the most environmentally friendly products and cleaning methods. At the same time, we are aware that many of our clients have environmental or chemical sensitivities. 
+            If you have concerns about any products that we may be using, please do not hesitate to ask us any questions. 
+            We will do our best to find a solution that will make everyone happy.            
+            </p>
+          </div>
+          <div className="md:w-1/2 mt-8 md:mt-0">
+            Picture Here
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -45,3 +45,31 @@ export const services = [
         title: "And many more!"
     }
 ]
+
+export const faq = [
+    {
+        id: "faq-1",
+        question: "How long does tailoring take?",
+        answer: "If you want to get things done as quickly as possible, call ahead for an appointment. Appointment duration for fittings can vary a lot, depending on the article of clothing. A wedding dress can take 45 minutes to an hour, while a pair of suit pants can be fitted in less than ten minutes."
+    },
+    {
+        id: "faq-2",
+        question: "What are the benefits of dry cleaning?",
+        answer: "Dry cleaning is a very effective cleaning process that can remove stains and dirt from clothing and other fabrics without risking shrinkage or damage. Traditional laundry services, using soap and water, can shrink, damage, or discolor clothing, while dry cleaning just cleans while protecting the clothing. For some fabrics and garments, dry cleaning is the only safe option."
+    },
+    {
+        id: "faq-3",
+        question: "Do you use any hazardous or polluting cleaning products?",
+        answer: "No. We use environmentally-friendly, totally biodegradable soaps and conditioners to get your garments clean without negatively affecting the environment. We can safely clean “dry clean only” items without the harmful solvents and chemicals that are often used by other dry cleaners."
+    },
+    {
+        id: "faq-4",
+        question: "Does frequent dry cleaning shorten the life of a garment?",
+        answer: "No. On the contrary, dry cleaning actually extends the life of garments, because unlike water washing, dry cleaning does not slowly degrade the quality of the clothing. Instead, it removes dirt and stains, and can restore and preserve the integrity of the garment."
+    },
+    {
+        id:"faq-5",
+        question: "Have questions you don't see here?",
+        answer: "Call or submit a form in our Contact Us section above!"
+    }
+]
